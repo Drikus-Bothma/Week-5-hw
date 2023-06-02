b4c9787f0dcf0fcc49b94c866570a447fcbad86e
Drikus-Bothma <231155@virtualwindow.co.za>
2023-06-02 16:05:27 +0200

    done

---
+++ js/index.js
@@ -0,0 +1,11 @@
+logVehicle = () => {
+    let type = document.getElementById("vType").value
+    let make = document.getElementById("make").value
+    let model = document.getElementById("model").value
+    let res = +document.getElementById("resNumber").value
+
+    alert(type + "hghfkr" + make + " ,jfbksdb" + model + " ldhfksd" + res)
+
+    document.getElementById("form").reset
+
+}
